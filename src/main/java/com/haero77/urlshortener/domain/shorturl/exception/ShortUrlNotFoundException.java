ed7a4e54493750fccf5f3a09140b362ce4e43d79
Seonho Kim <sunhowold@naver.com>
2023-04-30 18:39:22 +0900

    #3 Feat: short url을 통해 origin url을 찾는 로직 구현

---
+++ src/main/java/com/haero77/urlshortener/domain/shorturl/exception/ShortUrlNotFoundException.java
@@ -0,0 +1,12 @@
+package com.haero77.urlshortener.domain.shorturl.exception;
+
+import org.springframework.http.HttpStatus;
+
+import com.haero77.urlshortener.global.errors.exception.BusinessException;
+
+public class ShortUrlNotFoundException extends BusinessException {
+
+	public ShortUrlNotFoundException(String message) {
+		super(message, HttpStatus.NOT_FOUND);
+	}
+}
