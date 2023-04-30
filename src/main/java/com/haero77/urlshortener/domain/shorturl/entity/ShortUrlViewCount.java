07443496de8e020b980df72beae70d95e5b5201a
Seonho Kim <sunhowold@naver.com>
2023-04-30 20:57:34 +0900

    #1 Feat: Url의 조회수를 기록하는 ShortUrlViewCount 도메인 객체 구현

--- src/main/java/com/haero77/urlshortener/domain/shorturl/entity/ShortUrlViewCount.java
+++ src/main/java/com/haero77/urlshortener/domain/shorturl/entity/ShortUrlViewCount.java
@@ -1,15 +1,37 @@
 package com.haero77.urlshortener.domain.shorturl.entity;
 
+import static javax.persistence.FetchType.*;
 import static javax.persistence.GenerationType.*;
+
+import java.time.LocalDate;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.haero77.urlshortener.domain.shorturl.type.BaseTime;
 
 @Entity
-public class ShortUrlViewCount {
+public class ShortUrlViewCount extends BaseTime {
 
 	@Id
 	@GeneratedValue(strategy = IDENTITY)
 	private Long id;
+
+	@ManyToOne(fetch = LAZY)
+	@JoinColumn(name = "short_url_id")
+	private ShortUrl shortUrl;
+
+	private LocalDate localDate;
+
+	private int viewCount;
+
+	protected ShortUrlViewCount() {
+	}
+
+	public ShortUrlViewCount(ShortUrl shortUrl) {
+		this.shortUrl = shortUrl;
+	}
 }
