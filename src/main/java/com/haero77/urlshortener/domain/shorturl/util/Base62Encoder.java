761ee6bcddfeda04cde1e263a1045524b4119fde
Seonho Kim <sunhowold@naver.com>
2023-04-30 16:59:44 +0900

    #1 Feat: 숫자를 62진법으로 변환하는 인코더 객체 구현

---
+++ src/main/java/com/haero77/urlshortener/domain/shorturl/util/Base62Encoder.java
@@ -0,0 +1,22 @@
+package com.haero77.urlshortener.domain.shorturl.util;
+
+public class Base62Encoder {
+
+	private static final int BASE = 62;
+	private static final String CODEC = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	private Base62Encoder() {
+	}
+
+	public static String encode(long target) {
+		StringBuilder encoded = new StringBuilder();
+
+		do {
+			int mod = Math.toIntExact(target % BASE);
+			encoded.insert(0, CODEC.charAt(mod));
+			target /= BASE;
+		} while (target != 0);
+
+		return encoded.toString();
+	}
+}
