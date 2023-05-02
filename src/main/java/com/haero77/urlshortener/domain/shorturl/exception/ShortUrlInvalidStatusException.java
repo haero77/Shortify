30b1422616b1691194418a51b19d03e218447cb8
Seonho Kim <sunhowold@naver.com>
2023-05-03 02:19:04 +0900

    #4 Refactor: origin url 조회 시 유효한 상태의 url만 조회할 수 있도록 변경

---
+++ src/main/java/com/haero77/urlshortener/domain/shorturl/exception/ShortUrlInvalidStatusException.java
@@ -0,0 +1,11 @@
+package com.haero77.urlshortener.domain.shorturl.exception;
+
+import org.springframework.http.HttpStatus;
+
+import com.haero77.urlshortener.global.errors.exception.BusinessException;
+
+public class ShortUrlInvalidStatusException extends BusinessException {
+	public ShortUrlInvalidStatusException(String message) {
+		super(message, HttpStatus.BAD_REQUEST);
+	}
+}
