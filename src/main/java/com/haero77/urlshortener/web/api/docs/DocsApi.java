fee8137f811a97ec749fabfc69464b20532030c0
Seonho Kim <sunhowold@naver.com>
2023-05-04 05:12:46 +0900

    Docs: Update REST Docs

--- src/main/java/com/haero77/urlshortener/web/api/docs/DocsApi.java
+++ src/main/java/com/haero77/urlshortener/web/api/docs/DocsApi.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DocsApi {
 
-	@GetMapping("/docs/api")
+	@GetMapping("/api/docs")
 	public ResponseEntity<String> docs() {
 		URI docsPath = URI.create("/docs/index.html");
 
