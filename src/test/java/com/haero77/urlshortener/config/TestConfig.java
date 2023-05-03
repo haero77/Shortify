951d62c7f22655ba61b35c10aefe67dafbd1a2e6
Seonho Kim <sunhowold@naver.com>
2023-05-04 05:12:46 +0900

    Docs: Update REST Docs

--- src/test/java/com/haero77/urlshortener/config/TestConfig.java
+++ src/test/java/com/haero77/urlshortener/config/TestConfig.java
@@ -3,9 +3,11 @@ package com.haero77.urlshortener.config;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 
+import com.haero77.urlshortener.domain.url.repository.UrlCallRepository;
 import com.haero77.urlshortener.domain.url.repository.UrlRepository;
 import com.haero77.urlshortener.domain.url.service.UrlCreator;
 import com.haero77.urlshortener.domain.url.service.UrlReader;
+import com.haero77.urlshortener.domain.url.service.UrlStatisticsProvider;
 import com.haero77.urlshortener.domain.url.service.UrlUpdater;
 
 @TestConfiguration
@@ -32,5 +34,13 @@ public class TestConfig {
 		UrlReader urlReader
 	) {
 		return new UrlUpdater(urlReader);
+	}
+
+	@Bean
+	public UrlStatisticsProvider urlStatisticsProvider(
+		UrlCallRepository urlCallRepository,
+		UrlReader urlReader
+	) {
+		return new UrlStatisticsProvider(urlCallRepository, urlReader);
 	}
 }
--- src/test/java/com/haero77/urlshortener/web/api/url/UrlDeleteApiTest.java
+++ src/test/java/com/haero77/urlshortener/web/api/url/UrlDeleteApiTest.java
@@ -2,9 +2,11 @@ package com.haero77.urlshortener.web.api.url;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.*;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
-import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.time.LocalDateTime;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,23 +14,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.haero77.urlshortener.domain.url.dto.UrlCreateRequest;
+import com.haero77.urlshortener.domain.url.entity.Url;
 import com.haero77.urlshortener.domain.url.repository.UrlRepository;
+import com.haero77.urlshortener.domain.url.util.TimeUtil;
 import com.haero77.urlshortener.restdocs.ApiDocumentUtils;
 
 @AutoConfigureMockMvc
 @AutoConfigureRestDocs
 @SpringBootTest
 @Transactional
-class UrlCreateApiTest {
-
-	private final ObjectMapper objectMapper = new ObjectMapper();
+class UrlDeleteApiTest {
 
 	@Autowired
 	private MockMvc mockMvc;
@@ -37,33 +36,30 @@ class UrlCreateApiTest {
 	private UrlRepository urlRepository;
 
 	@Test
-	@DisplayName("단축 URL 생성")
-	void create() throws Exception {
-		// given
-		String originUrl = "https://github.com/haero77";
-		boolean expirationOption = false;
-		UrlCreateRequest urlCreateRequest = new UrlCreateRequest(originUrl, expirationOption);
+	@DisplayName("단축 URL 삭제")
+	void url_delete() throws Exception {
+
+		LocalDateTime currentDate = TimeUtil.getCurrentSeoulTime();
+		Url url = Url.createWithoutShortenedUrl(
+			"https://github.com/haero77", Url.DEFAULT_EXPIRATION_PERIOD, currentDate.minusDays(7));
+		String shortenedUrl = "shortenedUrl";
+		url.assignShortenedUrl(shortenedUrl);
+
+		Url savedUrl = urlRepository.save(url);
 
 		// when & then
-		ResultActions resultActions = mockMvc.perform(post("/api/urls")
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(objectMapper.writeValueAsString(urlCreateRequest)))
-			.andExpect(status().isCreated());
+		ResultActions resultActions = mockMvc.perform(
+				delete("/api/urls/{urlId}", savedUrl.id()))
+			.andExpect(status().isNoContent());
 
 		// REST Docs
 		resultActions
 			.andDo(print())
-			.andDo(document("short-url-create",
+			.andDo(document("short-url-delete",
 				ApiDocumentUtils.getDocumentRequest(),
 				ApiDocumentUtils.getDocumentResponse(),
-				requestFields(
-					fieldWithPath("url").description("단축할 URL"),
-					fieldWithPath("expiration").description("만료 옵션 지정 여부")
-				),
-				responseFields(
-					fieldWithPath("id").description("단축된 URL ID"),
-					fieldWithPath("shortenedUrl").description("단축된 URL"),
-					fieldWithPath("originUrl").description("원본 URL")
+				pathParameters(
+					parameterWithName("urlId").description("단축된 URL ID")
 				)));
 	}
 }
